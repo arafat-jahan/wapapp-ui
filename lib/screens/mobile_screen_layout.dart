5f3cd2917ea3aca17e5810e9ab468b62a1ec81df
arafat-jahan <ajh.official15@gmail.com>
2026-01-02 23:04:48 +0600

    new

---
+++ lib/screens/mobile_screen_layout.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: 3, child: Scaffold(
+      appBar: AppBar(
+        title: const Text('WhatsApp', style: TextStyle(color: Colors.grey, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(onPressed:(){}, icon:Icon(Icons.search, color: Colors.grey,)),
+          IconButton(onPressed:(){}, icon:Icon(Icons.more_vert,color: Colors.grey,))
+
+        ],
+        bottom: const TabBar(
+
+            tabs: [
+          Tab(text: 'CHATS',),
+          Tab(text: 'STATUS',),
+          Tab(text: 'CALLS',),
+
+        ]),
+      ),
+    ),
+    );
+  }
+}
